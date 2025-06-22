8386f12decb655287e05b6829aaafcf0b7d19147
lomeat <lom3at@gmail.com>
2025-06-23 01:29:45 +0300

    spawn animtion for dropped item

--- items/item.gd
+++ items/item.gd
@@ -22,7 +22,7 @@ func take_damage(damage: int) -> void:
 	sprite.modulate = Color.DIM_GRAY
 	await get_tree().create_timer(0.1).timeout
 
-	if current_health >= 0:
+	if current_health > 0:
 		current_health -= damage
 
 	if current_health <= 0:
@@ -49,15 +49,35 @@ func drop_item():
 
 func spawn_item_in_random_pos(index: int, total: int):
 	var item = loot_item.instantiate()
-	var spread_radius := 128.0
+	var spread_radius := 264.0
 	var angle := rng.randf_range(0, TAU)
-	var offset := Vector2(cos(angle) * spread_radius / 2 * sqrt(float(index) / total), 
-						sin(angle) * spread_radius * sqrt(float(index) / total))
-	item.global_position = global_position + offset 
-
+	var offset := Vector2(
+		cos(angle) * spread_radius / 2 * sqrt(float(index) / total), 
+		sin(angle) * spread_radius * sqrt(float(index) / total)
+	)
+	item.global_position = global_position
+	var target_position = global_position + offset
+	
 	var parent = get_parent()
 	if parent:
 		parent.add_child(item)
+		await get_tree().process_frame
+		spawn_animation(item, target_position)
 	else:
 		printerr("There is no parent to spawn dropped item")
-		
+
+func spawn_animation(item: Node2D, target_position: Vector2):
+	var tween = item.create_tween()
+	tween.set_parallel(true)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_BACK)
+
+	tween.tween_property(item, "global_position", target_position, 0.5)
+	item.modulate.a = 0.5
+	tween.tween_property(item, "modulate:a", 1, 0.3)
+
+	if item.has_node("Shadow"):
+		var shadow = item.get_node("Shadow")
+		var original_a = shadow.modulate.a
+		shadow.modulate.a = 0.0
+		tween.tween_property(shadow, "modulate:a", original_a, 0.6)
--- items/item_drop.gd
+++ items/item_drop.gd
@@ -1,19 +1,21 @@
 class_name ItemDrop
 extends Area2D
 
-signal was_collected
+signal can_picked_up
 
 var is_can_picked_up := false
 
 func _ready() -> void:
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.5).timeout
 	is_can_picked_up = true
-
 
 func _on_body_entered(_body: Node2D) -> void:
 	var bodies = get_overlapping_bodies()
 
 	for body in bodies:
 		if is_can_picked_up and body is Player:
-			was_collected.emit()
-			queue_free()
+			can_picked_up.emit()
+
+func destroy() -> void:
+	queue_free()
+	# TODO: Animation also
