532175e5f6b05a625c4605c2c4e6dcc62ad18435
lomeat <lom3at@gmail.com>
2025-06-25 01:05:30 +0300

    dropped items when can be picked up highlights with outline (shader)

--- items/item_drop.gd
+++ items/item_drop.gd
@@ -9,6 +9,8 @@ var item_res: ItemResource
 var count: int = 1
 var animation: Tween = create_tween()
 var can_picked_up := false
+var back_sprite: Sprite2D
+var new_sprite: Sprite2D
 
 func setup(res: ItemResource, total: int):
 	if not res or not res.texture:
@@ -16,28 +18,32 @@ func setup(res: ItemResource, total: int):
 		return
 
 	if not sprite:
-		# push_error("There is somehow no sprite instance")
-		var new_sprite = Sprite2D.new()
+		new_sprite = Sprite2D.new()
 		new_sprite.texture = res.texture
 		new_sprite.position = Vector2.ZERO
 		add_child(new_sprite)
-		sprite = new_sprite
 	else:
 		sprite.texture = res.texture
 
 	item_res = res
 	count = total
-	
-	if has_node("Shadow"):
-		var shadow = $Shadow
-		shadow.texture = res.texture
-		shadow.modulate = Color(0, 0, 0, 0.3)
+
+	var shadow = $Shadow
+	shadow.texture = res.texture
+	shadow.modulate = Color(0, 0, 0, 0.3)
 
 
 func _ready() -> void:
+	var outline_material = ShaderMaterial.new()
+	outline_material.shader = load("res://items/outline.gdshader")
+	outline_material.set_shader_parameter("outline_width", 0.0)
+	outline_material.set_shader_parameter("outline_color", Color(1,1,1,0))
+	new_sprite.material = outline_material
+	
 	await get_tree().create_timer(0.5).timeout
 	can_picked_up = true
 	GlobalSignals.item_ready_pickup.emit(self)
+
 	
 func destroy():
 	await run_animation("collect")
@@ -63,13 +69,15 @@ func run_animation(state_anim := "idle"):
 
 func animate_ready_pick_up():
 	animation = create_tween()
-	animation.set_loops(0)
-	animation.tween_property(self, "scale", Vector2(1.2, 1.2), 0.5)
-	animation.tween_property(self, "scale", Vector2(1.0, 1.0), 0.5)
-
+	animation.set_parallel(true) 
+	animation.tween_property(new_sprite.material, "shader_parameter/outline_width", 4.0, 0.2).set_ease(Tween.EASE_IN)
+	animation.tween_property(new_sprite.material, "shader_parameter/outline_color", Color(1, 1, 1, 0.4), 0.2).set_ease(Tween.EASE_IN)
+	
 func animate_idle():
 	animation = create_tween()
-	animation.tween_property(self, "scale", Vector2.ONE, 0.1)
+	animation.set_parallel(true) 
+	animation.tween_property(new_sprite.material, "shader_parameter/outline_width", 0, 0.2).set_ease(Tween.EASE_IN)
+	animation.tween_property(new_sprite.material, "shader_parameter/outline_color", Color(1, 1, 1, 0), 0.2).set_ease(Tween.EASE_IN)
 
 func animate_collect(duration: float) -> void:
 	var player_pos = get_tree().get_first_node_in_group("player").global_position
@@ -78,4 +86,5 @@ func animate_collect(duration: float) -> void:
 	animation.tween_property(self, "global_position", player_pos, duration)
 	animation.tween_property(self, "scale", Vector2(1, 1), duration)
 	animation.tween_property(self, "modulate:a", 0.0, duration)
+
 
